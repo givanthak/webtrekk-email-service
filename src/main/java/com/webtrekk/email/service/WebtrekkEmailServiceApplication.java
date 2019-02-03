8939bd936917d650e69c202ca00ccac1dc063d32
givantha <prathapgivantha@gmail.com>
2019-02-03 20:20:10 +0530

    add initial commit

---
+++ src/main/java/com/webtrekk/email/service/WebtrekkEmailServiceApplication.java
@@ -0,0 +1,14 @@
+package com.webtrekk.email.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtrekkEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtrekkEmailServiceApplication.class, args);
+	}
+
+}
+
